f627f94b4c5249ae3d5a5907dac2057ac8b03d9f
Jamal Jama <750292+byjamaljama@users.noreply.github.com>
2021-10-22 11:48:23 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+mk_random_no_0_500() {
+  echo $(( $RANDOM % 500 ))
+}
+
+echo "################################"
+echo "## Downloading required files ##"
+echo "################################"
+
+cd ~
+
+if [ -e ~/.vimrc ]; then
+	echo "## Backingup old .vimrc, OK."
+  mv .vmrc .backup.vimrc.$RANDOM
+fi
+
+wget -O .vimrc https://raw.githubusercontent.com/byjamaljama/vim/main/.vimrc
+
+if [ -e ~/.vim/bundle ]; then
+	echo "## Backingup old Vundle, OK."
+  mv ~/.vim/bundle ~/.vim/bundle.backup.$RANDOM
+fi
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "################################"
+echo "## 2. Instructions            ##"
+echo "################################"
+
+echo "1. Open vim and run :PluginInstall"
+echo "2. cd ~/.vim/bundle/YouCompleteMe"
+echo "3. run python3 install.py --all"
